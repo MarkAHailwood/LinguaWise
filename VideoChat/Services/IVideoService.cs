13c2df2a7357d2bfe528fe28950824c42089f743
Mark Anthony Hailwood <mark.hailwood@cognisess.com>
2022-09-12 12:24:31 +0100

    Build backend

---
+++ VideoChat/Services/IVideoService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using VideoChat.Models;
+
+namespace VideoChat.Services
+{
+    public interface IVideoService
+    {
+        string GetTwilioJwt(string identity);
+        Task<IEnumerable<RoomDetails>> GetAllRoomsAsync();
+    }
+}
---
+++ VideoChat/Startup.cs
@@ -0,0 +1,74 @@
+﻿using VideoChat.Hubs;
+using VideoChat.Options;
+using VideoChat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace VideoChat
+{
+    public class Startup
+    {
+        readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration) => _configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.Configure<TwilioSettings>(settings =>
+            {
+                settings.AccountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+                settings.ApiSecret = Environment.GetEnvironmentVariable("TWILIO_API_SECRET");
+                settings.ApiKey = Environment.GetEnvironmentVariable("TWILIO_API_KEY");
+            })
+                    .AddTransient<IVideoService, VideoService>()
+                    .AddSpaStaticFiles(config => config.RootPath = "ClientApp/dist");
+
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection()
+               .UseStaticFiles()
+               .UseSpaStaticFiles();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+
+                endpoints.MapHub<NotificationHub>("/notificationHub");
+            })
+               .UseSpa(spa =>
+               {
+                   // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                   // see https://go.microsoft.com/fwlink/?linkid=864501
+                   spa.Options.SourcePath = "ClientApp";
+
+                   if (env.IsDevelopment())
+                   {
+                       spa.UseAngularCliServer(npmScript: "start");
+                       //spa.UseProxyToSpaDevelopmentServer(_configuration["SpaBaseUrl"] ?? "http://localhost:4200");
+                   }
+               });
+        }
+    }
+}
